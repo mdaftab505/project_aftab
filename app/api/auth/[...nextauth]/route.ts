7d0d46e97559643e0fd92d9d95374d4863d1bca1
mdaftab505 <mdaftab.quraishi1129@gmail.com>
2023-11-05 22:42:15 +0530

    sess

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -18,7 +18,7 @@ const handler = NextAuth({
 
 
             
-                const sessionUser = await User.findOne({email: session.user.email});
+                const sessionUser = await User.findOne({email:session?.user.email || ''});
     
                 session.user.id = sessionUser._id.toString();
         
