bab05a264941fc5b9b96dad5ddcbab89dd4982f9
mdaftab505 <mdaftab.quraishi1129@gmail.com>
2023-11-05 15:35:20 +0530

    google id fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,8 +8,8 @@ import User from '@models/user'
 const handler = NextAuth({
     providers:[
         GoogleProvider({
-            clientId : process.env.GOOGLE_ID, 
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+            clientId : process.env.GOOGLE_CLIENT_ID, 
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET
         })
     ],
 
