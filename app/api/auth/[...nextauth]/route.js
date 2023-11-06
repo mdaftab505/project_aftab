8f87b249b93ea9e880a76ce114f6a478dbe211b3
mdaftab505 <mdaftab.quraishi1129@gmail.com>
2023-11-06 21:37:42 +0530

    secret adeed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,59 +1,47 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-import { connectToDB } from '@utils/database'
-import User from '@models/user'
-
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId : process.env.GOOGLE_CLIENT_ID, 
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    secret:"vT5HRYtFi6uh6Du6cpick5weXPodF5EcEb1xEinEws0=",
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-    callbacks:{
-        async session({session}) {
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
 
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-            
-                const sessionUser = await User.findOne({email:session?.user?.email});
-    
-                session.user.id = sessionUser._id.toString(); // @ts-ignore 
-        
-                return session;
-                    
-            
-        },
-    
-        async signIn({profile, account,user,credentials})  {
-            try {
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-                await connectToDB();
-    
-                // check if user already exist
-                const userExists= await User.findOne({email:profile.email});     // @ts-ignore 
-              //if not create a new user
-              if(!userExists){
-                await User.create({
-                    email:profile.email, // @ts-ignore 
-                    username:profile.name.replace(" ", "").toLowerCase(),// @ts-ignore 
-                    image:profile.picture, // @ts-ignore 
-                });
-              }
-    
-              return true;
-                
-            } catch (error) {
-                console.log(error);
-                return false
-            }
-        },
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
+      }
+    },
+  }
+})
 
-    }
-   
-});
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
